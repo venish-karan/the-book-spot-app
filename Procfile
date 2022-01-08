aaab9ac1781f014bcdf0d272a9d98db17274d51d
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-09 00:04:40 +0530

    ready for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_book_spot.wsgi --log-file -
--- the_book_spot/settings.py
+++ the_book_spot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Herolu settings for Django
+django_heroku.settings(locals())
